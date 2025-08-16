b59e0b871d80034f7b0171ee8b3ab99380d60632
StarLord0031 <vaibhav18katkar@gmail.com>
2025-08-16 12:22:32 +0530

    'update'

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,4 +1,5 @@
-import { getLongUrl, storeClicks } from "@/db/apiUrls";
+import { getLongUrl } from "@/db/apiUrls";
+import storeClicks from "@/db/apiClicks";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BarLoader } from "react-spinners";
