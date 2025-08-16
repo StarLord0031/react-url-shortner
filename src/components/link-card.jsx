923590d0defce17f36df86160b8e010b71d164ac
StarLord0031 <vaibhav18katkar@gmail.com>
2025-08-16 11:23:37 +0530

    https://snipster.vercel.app/

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -40,7 +40,7 @@ const LinkCard = ({ url, fetchUrls }) => {
                     {url?.title}
                 </span>
                 <span className='text-2xl text-blue-400 font-bold hover:underline cursor-pointer'>
-                    https://cutyoururl.in/{url?.custom_url ? url?.custom_url : url?.short_url}
+                    https://snipster.vercel.app/{url?.custom_url ? url?.custom_url : url?.short_url}
                 </span>
                 <span className='flex items-center gap-1 hover:underline cursor-pointer'>
                     {url?.original_url}
@@ -52,7 +52,7 @@ const LinkCard = ({ url, fetchUrls }) => {
 
             <div className='flex gap-2'>
                 <Button variant="ghost" onClick={() =>
-                    navigator.clipboard.writeText(`https://cutyoururl.in/${url?.custom_url ? url?.custom_url : url?.short_url}`)
+                    navigator.clipboard.writeText(`https://snipster.vercel.app/${url?.custom_url ? url?.custom_url : url?.short_url}`)
                 }>
                     <Copy />
                 </Button>
--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const Link = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimrr.in/${link}`}
+            href={`https://snipster.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimrr.in/{link}
+            https://snipster.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const Link = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimrr.in/${link}`)
+                navigator.clipboard.writeText(`https://snipster.vercel.app/${link}`)
               }
             >
               <Copy />
